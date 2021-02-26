5e954e3cc85048c873dc0c596188495d81c653c3
Kyle Mayes <kyle@mayeses.com>
2021-02-25 19:03:01 -0500

    Add PowerShell script to apply patches to tutorial source files

---
+++ tutorial/src/patch.ps1
@@ -0,0 +1,21 @@
+if (-NOT ($args.Length -eq 3)) {
+    Write-Host "Expected original file, patched file, and prefix number to start applying patch to."
+    exit
+}
+
+$original = $args[0]
+$patched = $args[1]
+$start = [int]$args[2]
+
+& "C:\Program Files\Git\usr\bin\diff.exe" -Naur $original $patched | Out-File patch.patch
+
+Get-ChildItem . -Filter *.rs | Sort-Object | Foreach-Object {
+    $prefix = [int]$_.Name.Split("_")[0]
+    if ($prefix -ge $start) {
+        Get-Content patch.patch | & "C:\Program Files\Git\usr\bin\patch.exe" -f $_.Name
+        Remove-Item -ErrorAction Ignore ($_.Name + ".orig")
+        Remove-Item -ErrorAction Ignore ($_.Name + ".rej")
+    }
+}
+
+Remove-Item -ErrorAction Ignore patch.patch
