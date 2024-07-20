40ef40a18693c9e77db48034004f494f0942080d
Kyle Mayes <kyle@mayeses.com>
2024-07-20 17:33:46 -0400

    Add vulkanalia-vma crate

---
+++ ext/vma/src/lib.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+
+//! An integration of [Vulkan Memory Allocator](https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator) with [`vulkanalia`](https://github.com/KyleMayes/vulkanalia).
+
+#![no_std]
+
+/// Raw bindings to VMA.
+pub mod vma;
