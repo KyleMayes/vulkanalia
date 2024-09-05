f17d6d08c0e9303f3c120300c7608bd4c7be9c8c
Kyle Mayes <kyle@mayeses.com>
2024-09-04 20:15:01 -0400

    Add basic VMA example

--- ext/vma/src/lib.rs
+++ ext/vma/src/lib.rs
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! An integration of [Vulkan Memory Allocator](https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator) with [`vulkanalia`](https://github.com/KyleMayes/vulkanalia).
+//!
+//! ### Example
+//!
+//! ```
+//! use vulkanalia::prelude::v1_0::*;
+//! use vulkanalia_vma::{self as vma, Alloc};
+//!
+//! fn example(instance: &Instance, device: &Device, physical_device: vk::PhysicalDevice) {
+//!     // Create an allocator.
+//!     let allocator_options = vma::AllocatorOptions::new(instance, device, physical_device);
+//!     let allocator = unsafe { vma::Allocator::new(&allocator_options) }.unwrap();
+//!
+//!     // Allocate a buffer using the allocator.
+//!     let buffer_create_info = vk::BufferCreateInfo::builder()
+//!         .size(65_536)
+//!         .usage(vk::BufferUsageFlags::VERTEX_BUFFER)
+//!         .sharing_mode(vk::SharingMode::CONCURRENT);
+//!     let allocation_options = vma::AllocationOptions::default();
+//!     let (buffer, allocation) = unsafe { allocator.create_buffer(buffer_create_info, &allocation_options) }.unwrap();
+//!
+//!     // Deallocate the buffer using the allocator.
+//!     unsafe { allocator.destroy_buffer(buffer, allocation) };
+//! }
+//! ```
 
 #![no_std]
 #![allow(clippy::missing_safety_doc)]
