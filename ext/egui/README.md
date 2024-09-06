8723020ae85d7ea903903887f13f492b536a7ab1
Kyle Mayes <kyle@mayeses.com>
2024-09-05 22:39:06 -0400

    Add vulkanalia-egui crate

---
+++ ext/egui/README.md
@@ -0,0 +1,5 @@
+# vulkanalia-egui
+
+An integration of [`egui`](https://github.com/emilk/egui) with [`vulkanalia`](https://github.com/KyleMayes/vulkanalia).
+
+Released under the Apache License 2.0.
---
+++ ext/egui/src/lib.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+
+//! An integration of [`egui`](https://github.com/emilk/egui) with [`vulkanalia`](https://github.com/KyleMayes/vulkanalia).
+
+#![no_std]
+#![allow(clippy::missing_safety_doc)]
+
+extern crate alloc;
