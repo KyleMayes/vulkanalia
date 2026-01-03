aea11d0a2cf652f2a4ef8720fafeadfec983f763
Kyle Mayes <kyle@mayeses.com>
2026-01-02 22:04:31 -0500

    Add include_shader_code! macro

--- vulkanalia/src/bytecode.rs
+++ vulkanalia/src/bytecode.rs
@@ -9,6 +9,46 @@ use core::error;
 #[cfg(feature = "std")]
 use std::error;
 
+/// A wrapper around [`include_bytes!`] that includes a SPIR-V bytecode buffer.
+///
+/// SPIR-V bytecode is required to be a sequence of 32-bit words, so this macro
+/// enforces that the length of the included bytes is a multiple of 4 and stores
+/// the included bytes in a `#[repr(C, align(4))]` struct before converting the
+/// included bytes into a `&[u32]` to ensure valid alignment.
+///
+/// ### Example
+///
+/// ```
+/// # use vulkanalia::include_shader_code;
+/// # use vulkanalia::prelude::v1_0::*;
+/// const vert_bytecode: &[u32] = include_shader_code!("../../examples/shaders/default/vert.spv");
+/// let vert_info = vk::ShaderModuleCreateInfo::builder()
+///     .code_size(vert_bytecode.len() * 4)
+///     .code(vert_bytecode);
+/// ```
+#[macro_export]
+macro_rules! include_shader_code {
+    ($file:expr $(,)?) => {
+        const {
+            #[repr(C, align(4))]
+            struct Align<Bytes: ?Sized>(Bytes);
+            static BYTES: &Align<[u8]> = &Align(*include_bytes!($file));
+
+            if BYTES.0.len() % 4 != 0 {
+                panic!(concat!(
+                    "length of included SPIR-V bytecode file (",
+                    $file,
+                    ") is not a multiple of 4"
+                ));
+            }
+
+            let data: *const u32 = BYTES.0.as_ptr().cast();
+            let len = BYTES.0.len() / 4;
+            unsafe { ::core::slice::from_raw_parts(data, len) }
+        }
+    };
+}
+
 /// An error raised by a failure to construct a [`Bytecode`].
 #[derive(Clone, Debug, PartialEq)]
 pub enum BytecodeError {
@@ -31,11 +71,18 @@ impl fmt::Display for BytecodeError {
 #[cfg(any(feature = "std", feature = "no_std_error"))]
 impl error::Error for BytecodeError {}
 
-/// A 4-byte aligned SPIR-V bytecode buffer.
+/// A 4-byte (or higher) aligned SPIR-V bytecode buffer.
 ///
 /// This helper struct can be used to ensure the correct alignment of the SPIR-V
-/// bytecode for a shader before providing it to a
+/// bytecode for a dynamically loaded shader before providing it to a
 /// [`crate::vk::ShaderModuleCreateInfo`] to create a shader module.
+///
+/// This helper struct is most useful for handling dynamically loaded SPIR-V
+/// bytecode. If you only want to include a file like in the below example,
+/// the [`include_shader_code!`] macro accomplishes the same goal as this helper
+/// struct but at compiletime and without any allocations.
+///
+/// ### Example
 ///
 /// ```
 /// # use vulkanalia::bytecode::Bytecode;
